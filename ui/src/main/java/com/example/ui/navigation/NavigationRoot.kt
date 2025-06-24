994c91ad3a38d91db50f60417bd906de0cfa5ae9
kikepb7 <enriquepalmadev@gmail.com>
2025-06-24 21:58:40 +0200

    NAVIGATION | Implement navigation 3 and set routes

--- ui/src/main/java/com/example/ui/navigation/NavigationRoot.kt
+++ ui/src/main/java/com/example/ui/navigation/NavigationRoot.kt
@@ -8,7 +8,16 @@ import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.runtime.rememberSavedStateNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
 import androidx.navigation3.ui.rememberSceneSetupNavEntryDecorator
+import com.example.domain.feature.club.model.ClubModel
+import com.example.domain.feature.user.model.UserModel
+import com.example.ui.feature.dashboard.DashboardScreenView
+import com.example.ui.feature.login.LoginScreenView
+import com.example.ui.feature.signup.SignUpScreenView
+import com.example.ui.feature.register.RegisterScreenView
 import com.example.ui.navigation.HomeNavKeys.HomeScreen
+import com.example.ui.navigation.HomeNavKeys.LoginScreen
+import com.example.ui.navigation.HomeNavKeys.RegisterScreen
+import com.example.ui.navigation.HomeNavKeys.DashboardScreen
 
 @Composable
 fun NavigationRoot(modifier: Modifier = Modifier) {
@@ -25,7 +34,24 @@ fun NavigationRoot(modifier: Modifier = Modifier) {
         ),
         entryProvider = { key ->
             when (key) {
-                is HomeScreen -> NavEntry(key = key) { }
+                is HomeScreen -> NavEntry(key = key) {
+                    LoginScreenView(
+                        onSignUpClick = { backStack.add(LoginScreen) },
+                        onRegisterClick = { backStack.add(RegisterScreen)}
+                    )
+                }
+                is LoginScreen -> NavEntry(key = key) {
+                    SignUpScreenView(
+                        onSignUpClick = { backStack.add(DashboardScreen(UserModel(), ClubModel())) },
+                        onRegisterClick = { backStack.add(HomeScreen)}
+                    )
+                }
+                is RegisterScreen -> NavEntry(key = key) {
+                    RegisterScreenView(onSuccessNavigate = { backStack.add(DashboardScreen(UserModel(), ClubModel())) })
+                }
+                is DashboardScreen -> NavEntry(key = key) {
+                    DashboardScreenView(user = key.user, club = key.club)
+                }
                 else -> throw RuntimeException("Unknown NavKey: $key")
             }
         }
--- ui/src/main/java/com/example/ui/navigation/Routes.kt
+++ ui/src/main/java/com/example/ui/navigation/Routes.kt
@@ -1,9 +1,25 @@
 package com.example.ui.navigation
 
 import androidx.navigation3.runtime.NavKey
+import androidx.versionedparcelable.VersionedParcelize
+import com.example.domain.feature.club.model.ClubModel
+import com.example.domain.feature.user.model.UserModel
 import kotlinx.serialization.Serializable
 
 object HomeNavKeys {
     @Serializable
     object HomeScreen : NavKey
+
+    @Serializable
+    object LoginScreen : NavKey
+
+    @Serializable
+    object RegisterScreen : NavKey
+
+    @VersionedParcelize
+    data class DashboardScreen(
+        val user: UserModel,
+        val club: ClubModel
+    ) : NavKey
 }
+
